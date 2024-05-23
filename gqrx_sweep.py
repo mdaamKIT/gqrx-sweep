0d74ef9123110e6b63183d8a93f3558c2d1a018f
lemique17 <michael.daam@kit.edu>
2024-05-23 15:10:31 +0200

    sweep fully functional, slider functional, minor changes to gui, handling of failed telnet connection introduced

--- gqrx_sweep.py
+++ gqrx_sweep.py
@@ -1,6 +1,6 @@
 
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QTimer, QLocale
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSpinBox, QSlider, QMessageBox
 from PyQt5.uic import loadUi
 
 import os
@@ -11,50 +11,97 @@ import telnetlib
 import time
 
 
-connect = True  # remove later # telnetlib.Telnet returns ConnetctionRefusedError, if nobody listens. (Could also use try/except.)
-
 class MainWindow(QMainWindow):
 
 	def __init__(self, configfile):
+
 		super().__init__()
 		loadUi(os.getcwd()+'/gqrx_sweep.ui',self)
+		self.setWindowTitle('gqrx sweeper')
 
 		### load config
 		self.config = ConfigParser()
 		self.config.read(configfile)
 		# main
 		self.debugmode = self.config.getboolean('main', 'debugmode')
-		# telnet		
-		if connect: self.tn = telnetlib.Telnet(self.config.get('telnet', 'HOST'), self.config.getint('telnet', 'PORT'))
+		self.setLocale(QLocale(QLocale.C))  # dot as decimal separator
 		# sweep
 		self.centerfreq = self.config.getint('sweep', 'centerfreq')
-		self.start = self.config.getint('sweep', 'start')
-		self.stop = self.config.getint('sweep', 'stop')
-		self.timestep = self.config.getfloat('sweep', 'timestep')
 		self.freqstep = self.config.getint('sweep', 'freqstep')
-		self.keyboard_control = self.config.getboolean('sweep', 'keyboard_control')
 
-		### connect PushButtons
-		self.pushButton_startsweep.clicked.connect(self.sweep_start)
-		self.pushButton_stopsweep.clicked.connect(self.sweep_interrupt)
+		### start Telnet TCP connection
+		self.connection = False
+		self.try_to_connect = True
+		while self.try_to_connect:
+			try:
+				self.tn = telnetlib.Telnet(self.config.get('telnet', 'HOST'), self.config.getint('telnet', 'PORT'))
+				self.connection = True
+				self.try_to_connect = False
+			except ConnectionRefusedError:
+				answer = QMessageBox.question(
+					self,
+					"TCP connection failed", 
+					'Connecting to gqrx via Telnet TCP failed.\nBefore trying again, make sure to have gqrx open, enable remote control, and make sure gqrx remote control settings match the settings in config.ini.\nYou can try again now, or close the application and restart it later.\n\nDo you want to try again now?',
+					QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+					)
+				if answer==QMessageBox.StandardButton.No:
+					self.try_to_connect = False
+					self.connection = False
+					print('Operation without Telnet TCP connection. (Should be quite useless.)')
 
-		### setup the sweep 
-		self.timer = QTimer() # timer to trigger next step (sweep)
+		### setup timer for sweep (triggers next step)
+		self.timer = QTimer()
 		self.timer.timeout.connect(self.step_up)
 		self.interrupt = False
 
+		### connect gui elements with functions
+		# spinBox for center frequency
+		self.spinBox_mhz.setRange(self.config.getint('sweep', 'minfreq'), self.config.getint('sweep', 'maxfreq'))
+		self.spinBox_mhz.setValue(self.centerfreq)
+		self.send_freq()
+		self.spinBox_mhz.valueChanged.connect(self.centerfreq_changed)
+		# horizontalSlider for center frequency
+		self.horizontalSlider_mhz.setRange(self.config.getint('sweep', 'minfreq'), self.config.getint('sweep', 'maxfreq'))
+		self.horizontalSlider_mhz.setValue(self.centerfreq)
+		self.horizontalSlider_mhz.setTickPosition(QSlider.TicksBelow)
+		self.horizontalSlider_mhz.setTickInterval(500)
+		self.horizontalSlider_mhz.sliderReleased.connect(self.slider_released)
+		# spinBoxes for sweep
+		self.spinBox_start.setRange(self.config.getint('sweep', 'minfreq'), self.config.getint('sweep', 'maxfreq'))
+		self.spinBox_start.setValue(self.config.getint('sweep', 'start'))
+		self.spinBox_stop.setRange(self.config.getint('sweep', 'minfreq'), self.config.getint('sweep', 'maxfreq'))
+		self.spinBox_stop.setValue(self.config.getint('sweep', 'stop'))
+		self.doubleSpinBox_timestep.setRange(0., 60.)  # hardcoded, since a change should really be unnecessary.
+		self.doubleSpinBox_timestep.setDecimals(1)
+		self.doubleSpinBox_timestep.setSingleStep(0.1)
+		self.doubleSpinBox_timestep.setValue(self.config.getfloat('sweep', 'timestep'))
+		# PushButtons
+		self.pushButton_startsweep.clicked.connect(self.sweep_start)
+		self.pushButton_stopsweep.clicked.connect(self.sweep_interrupt)
+
+
+	def centerfreq_changed(self):
+		self.centerfreq = self.spinBox_mhz.value()
+		self.horizontalSlider_mhz.setValue(self.centerfreq)
+		self.send_freq()
+
+	def slider_released(self):
+		self.centerfreq = self.horizontalSlider_mhz.value()
+		self.spinBox_mhz.setValue(self.centerfreq)
+
 	def sweep_start(self):
 		self.interrupt = False
-		timestep_ms = int(self.timestep*1000)
+		timestep_ms = int(self.doubleSpinBox_timestep.value()*1000)
 		self.timer.start(timestep_ms)  # in ms - 1000 means the timer triggers once every second
-		self.freq = self.start
+		self.centerfreq = self.spinBox_start.value()
 
 	def step_up(self):
 		'Sweep through the frequency range.'
 		if not self.interrupt:
-			if self.freq <= self.stop:
-				self.send_freq()
-				self.freq += self.freqstep
+			if self.centerfreq <= self.spinBox_stop.value():
+				self.horizontalSlider_mhz.setValue(self.centerfreq)
+				self.spinBox_mhz.setValue(self.centerfreq)
+				self.centerfreq += self.freqstep
 			else:
 				self.timer.stop()	
 		else:
@@ -64,10 +111,12 @@ class MainWindow(QMainWindow):
 		self.interrupt = True
 
 	def send_freq(self):
-		command = 'F '+str(int(self.freq*1e6))+'\r\n'
+		command = 'F '+str(int(self.centerfreq*1e6))+'\r\n'
 		if self.debugmode: print(command)
-		if connect: self.tn.write((command.encode('ascii')))
-
+		if self.connection: 
+			self.tn.write((command.encode('ascii')))
+		else:
+			print('Command not send due to lacking connection.')
 
 
 
